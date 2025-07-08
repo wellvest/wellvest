20ff6b8ac23cb5b78ce94de0cddb7f6a4bf95347
wellvest <wellvestltd@gmail.com>
2025-07-08 21:09:20 +0000

    map

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -4,14 +4,14 @@ const GoogleMap = () => {
       <div className="container mx-auto px-4">
         <div className="rounded-2xl overflow-hidden shadow-hero animate-fade-in">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.4063475625208!2d78.09662117554866!3d29.910201774991418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3909491f485dd999%3A0x7f417c753c0e944a!2swellvest!5e0!3m2!1sen!2sin!4v1716727514586!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.3030245530825!2d77.11402491508555!3d28.647446982412186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d0375c4bc3c7d%3A0x8b09b97a1afb71b4!2sRajouri%20Garden%2C%20Delhi!5e0!3m2!1sen!2sin!4v1657123456789!5m2!1sen!2sin"
             width="100%"
             height="450"
             style={{ border: 0 }}
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-            title="wellvest Office Location"
+            title="wellvest Rajouri Garden Office Location"
             className="w-full"
           />
         </div>
